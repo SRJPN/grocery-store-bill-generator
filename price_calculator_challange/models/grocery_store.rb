98d9e02c21c8825f78938f289316f383014cb5af
Sooraj Parameswaran <srjpn123@gmail.com>
2021-09-23 19:04:18 +0530

    Created GroceryStore and moved PurchasedItem to models

---
+++ price_calculator_challange/models/grocery_store.rb
@@ -0,0 +1,28 @@
+class GroceryStore
+    def initialize
+        milk = PurchasedItem.new('Milk', 3.97, true, 2, 5.00)
+        bread = PurchasedItem.new('Bread', 2.17, false, 3, 6.00)
+        banana = PurchasedItem.new('Banana', 0.99, false)
+        apple = PurchasedItem.new('Apple', 0.89, false)
+
+        @pricing_table = {
+            'milk' => milk,
+            'bread' => bread,
+            'banana' => banana,
+            'apple' => apple
+        }
+    end
+
+    def generate_bill purchase_list
+        bill = []
+
+        purchase_list.each do |key, value| 
+            bill.push ({
+                'item' => key, 
+                'price' => (@pricing_table[key.downcase].calculate_cost (value)),
+                'quantity' => value
+            })
+        end
+        bill
+    end
+end
--- price_calculator_challange/solution.rb
+++ price_calculator_challange/solution.rb
@@ -1,33 +1,18 @@
-require_relative 'PurchasedItem'
+require_relative 'models/purchased_item'
+require_relative 'Models/grocery_store'
 
 purchase_list = %w(milk milk bread banana bread bread bread milk apple)
 
-
-milk = PurchasedItem.new('Milk', 3.97, true, 2, 5.00)
-bread = PurchasedItem.new('Bread', 2.17, false, 3, 6.00)
-banana = PurchasedItem.new('Banana', 0.99, false)
-apple = PurchasedItem.new('Apple', 0.89, false)
-
-pricing_table = {
-    'milk' => milk,
-    'bread' => bread,
-    'banana' => banana,
-    'apple' => apple
-}
-
-bill = {}
-
-purchase_list.each do |item|
+list = purchase_list.inject({}) do |bill, item|
     # bill.index {|element| element == 'bar' }
     if bill.has_key? item
-        bill[item]['quantity'] = bill[item]['quantity'] + 1
+        bill[item] = bill[item] + 1
     else
-        bill[item] = {'quantity' => 1}
+        bill[item] = 1
     end
+    bill
 end
 
-bill.each do |key, value| 
-    value['price'] = pricing_table[key].calculate_cost value['quantity']
-end
-
+store = GroceryStore.new
+bill = store.generate_bill list
 puts bill
